48345c2a49ea20ad3ea0ed03521ed8ee6bfe63f4
selmakilic <selmakilic91@gmail.com>
2023-08-04 11:13:31 +0300

    ubuntu linux

---
+++ script.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+
+##pwd
+##echo "Hello World!"
+
+ ##wget https://www.runlog.com.tr/uploads/nutuk.pdf
+
+
+#degisken="techpro"
+#echo $degisken
+
+
+# 1  turkce karakter kullanma 
+# 2  harf alphanumeric karekterler ve underscore " " kullanilabilir
+# 3  var1 gecerli var% gecersiz 
+# 4  a-Z 0-9 _  kullanilabilir
+# 3 degisken isimleri sayiyla baslayamaz
+#0selma diye bir degisken olusturalmaz
+# 4 local degiskenler buyuk harf kullanilmaz
+
+#var1="world!"
+#echo "Hello" $var1
+1var1="world!"
+echo "Hello" $1var1
