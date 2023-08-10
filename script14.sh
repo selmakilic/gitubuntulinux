06ff87f272970a93db8a43c439e5b192a9495110
selmakilic <selmakilic91@gmail.com>
2023-08-10 18:39:59 +0300

    gitbash son ders

---
+++ script14.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##function
+
+function fun1 () {
+
+    echo $1
+}
+
+fun1 15 selma seda
+
+
+function fun2 () {
+
+    date 
+}
+
+fun2
+
+function fun3 () {
+
+    pwd  
+    echo "hello world  "
+ 
+}
+
+fun3
